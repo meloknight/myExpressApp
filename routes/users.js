1d7d0a49fb2f86f22aeb98e9049f1e7f32c984e1
brody <49502879+meloknight@users.noreply.github.com>
2024-04-25 17:04:02 -0700

    testing route changes for redeployment on azure web apps

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const app = express();
+app.use(express.json());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  console.log("Hello updates");
+  res.status(200).json(req.body);
+});
+
+// GET specific user by ID
+router.get("/:id", function (req, res, next) {
+  const { id } = req.params;
+  console.log(`Hello user with id: ${req.params}`);
+  res.status(200).json(req.params);
 });
 
 module.exports = router;
